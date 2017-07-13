e1d233d1da002c6945191a50ff8c53a347921eff
Avni <pinkpiano2000@gmail.com>
2017-07-13 15:40:21 -0400

    new page

---
+++ index.js
@@ -0,0 +1,15 @@
+<script>
+	$(window).scroll(function() {
+		$('#animatedElement').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+	});
+	$('#animatedElement').click(function() {
+		$(this).addClass("slideUp");
+	});
+</script>
